8d6c4e4056ba6615bbb84367aeb81682947b9b78
jkrouse13 <jon.krouse@gmail.com>
2016-10-21 14:13:42 -0400

    more work on the non functioning search bar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
     if params[:search]
       @posts = Post.search(params[:search]).order("created_at DESC")
     else
-      @posts = Post.all.order('created_at DESC')
+      @posts = Post.all.sort_by {|post| post.created_at}
     end
   end
 
